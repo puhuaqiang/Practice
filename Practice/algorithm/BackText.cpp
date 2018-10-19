c2b8ba9fb99348db0d6fcd69577b25dcb0868420
puhuaqiang <puhuaqiang@126.com>
2018-10-19 14:32:44 +0800

    update

--- Practice/algorithm/BackText.cpp
+++ Practice/algorithm/BackText.cpp
@@ -1,31 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
-/// å›žæ–‡å­—ç¬¦ä¸²
+/// ÅÐ¶ÏÊÇ·ñÊÇ»ØÎÄ×Ö·û´®
+bool Palindrome(const char*);
+
 int main()
 {
     string str;
-    int flag = 1;
- 
     while (cin >> str)
     {
-        int l = str.length();
-        for (int i = 0, j = l-1; i <= j; i++,j--)
-        {
-            if (str[i] != str[j])
-            {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag)
+        if (Palindrome(str.data()))
             cout << "YES" << endl;
         else
             cout << "NO" << endl;
-        flag = true;
     }
     std::cin.ignore();
- 
     return 0;
+}
+
+bool Palindrome(const char* lpStr)
+{
+    if(NULL == lpStr){
+        return false;
+    }
+    bool flag = true;
+    int l = strlen(lpStr);
+    if(l == 0){
+        return false;
+    }
+    for (int i = 0, j = l-1; i <= j; i++,j--)
+    {
+        if (lpStr[i] != lpStr[j])
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 }
